b5ecaf635d26e3d0fe07ce851b6d78801fb3a401
João Leonardo <joaoleonardo9921@gmail.com>
2024-08-11 18:00:04 -0300

    create methods existsByUserName and existsByEmail

--- src/main/java/br/com/estud_io_api/repository/UserRepository.java
+++ src/main/java/br/com/estud_io_api/repository/UserRepository.java
@@ -2,8 +2,18 @@ package br.com.estud_io_api.repository;
 
 import br.com.estud_io_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.name = :name")
+    boolean existsByUserName(@Param("name") String name);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+
 }
