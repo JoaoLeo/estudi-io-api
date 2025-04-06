ab6a08e76940b1059d96bbcc705e826af0e932cb
João Leonardo <joaoleonardo9921@gmail.com>
2025-04-06 00:03:39 -0300

    Update UserRepository.java

--- src/main/java/br/com/estud_io_api/repository/UserRepository.java
+++ src/main/java/br/com/estud_io_api/repository/UserRepository.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.name = :name")
-    boolean existsByUserName(@Param("name") String name);
+    User findByEmail(String email);
 
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.email = :email")
     boolean existsByEmail(@Param("email") String email);
