00c144293348736c51e6c895d265d6c223769c8f
João Leonardo <joaoleonardo9921@gmail.com>
2025-04-05 23:54:40 -0300

    fix context path and hello world endpoint

--- src/main/java/br/com/estud_io_api/controller/DefaultController.java
+++ src/main/java/br/com/estud_io_api/controller/DefaultController.java
@@ -2,12 +2,14 @@ package br.com.estud_io_api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("public")
 public class DefaultController {
 
-    @GetMapping()
+    @GetMapping("hello-world")
     public ResponseEntity<String> getDefaultEndpoint(){
         return ResponseEntity.ok("Hello World!");
     }
