a9cd6a435b15f672d0849f80e9c6985e52ebc3d4
João Leonardo <joaoleonardo9921@gmail.com>
2025-04-06 10:50:48 -0300

    email settings

---
+++ src/main/java/br/com/estud_io_api/service/email/EmailService.java
@@ -0,0 +1,27 @@
+package br.com.estud_io_api.service.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendSimpleEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
+}
